5f14aec24e2ac334a081c7adf27d610f8866d04b
adakamahesh <adakamahesh@gmail.com>
2022-09-18 10:02:52 +0530

    add program of ascii codes

---
+++ ascii-code/ascii-code-program.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("enter the symbole");
+    scanf("%c",&ch);//it is used to read the values
+    printf("ascii value is: %d\n",ch);
+    return 0;
+}
