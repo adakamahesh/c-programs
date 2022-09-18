a9ae56ab33bf6b1c062b6d20441e725c87fde81e
adakamahesh <adakamahesh@gmail.com>
2022-09-18 18:08:18 +0530

    add the ternary operator

---
+++ operators/ternary-operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;//user input valus
+    printf("enter the a and b values");
+    scanf("%d%d",&a,&b);//read the a and b valus
+    a>b?printf("mahesh\n"):printf("jay\n");
+    //ternary operator condition if condation is true display first statement other wise display second statement  
+    return 0;
+}
+//program for ternary operator
